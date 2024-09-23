0f3ba870b2312934e786c6a32ec027ba8e82126f
James Willett <info@james-willett.com>
2024-09-23 20:54:37 +0100

    Adding code for logoTicker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
