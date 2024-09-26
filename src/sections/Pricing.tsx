36fe8829958e0ee7044af8c487675f79c12369b0
James Willett <info@james-willett.com>
2024-09-26 19:49:29 +0100

    Adding the keys for the previous code, to fix an error

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -75,6 +75,7 @@ export const Pricing = () => {
               features,
             }) => (
               <div
+                key={title}
                 className={twMerge(
                   "p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full",
                   inverse === true && "border-black bg-black text-white"
@@ -115,7 +116,10 @@ export const Pricing = () => {
                 </button>
                 <ul className="flex flex-col gap-5 mt-8">
                   {features.map((feature) => (
-                    <li className="text-sm flex items-center gap-4">
+                    <li
+                      key={feature}
+                      className="text-sm flex items-center gap-4"
+                    >
                       <CheckIcon className="h-6 w-6" />
                       <span>{feature}</span>
                     </li>
